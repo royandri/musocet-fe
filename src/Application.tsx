102b8de8e2879bbf5f2e97c457641b570a4a5811
naofalleoagusta <naofal.leo@sirclo.com>
2022-08-15 17:42:20 +0700

    add reducer for set score

--- src/Application.tsx
+++ src/Application.tsx
@@ -1,113 +1,107 @@
-import React, { useContext, useState, useRef, useEffect } from 'react';
-import SocketContext from './context/SocketContext';
+import React, { useContext, useState, useRef, useEffect } from "react";
+import SocketContext from "./context/SocketContext";
 
 export interface IApplicationProps {}
 
 const Application: React.FunctionComponent<IApplicationProps> = (props) => {
-    const { socket, user, users, song, pushedBell } = useContext(SocketContext).SocketState;
-    const dispatch = useContext(SocketContext).SocketDispatch;
-    const [name, setName] = useState("");
-    const [songSrc, setSongSrc] = useState("");
-    const audioRef = useRef<any>()
+  const { socket, user, users, song, pushedBell } =
+    useContext(SocketContext).SocketState;
+  const dispatch = useContext(SocketContext).SocketDispatch;
+  const [name, setName] = useState("");
+  const [songSrc, setSongSrc] = useState("");
+  const audioRef = useRef<any>();
 
-    const handleOnChangeName = () => {
-        if(socket) {
-            socket.emit("set_name", {
-                userId: user.userId,
-                name: name,
-            })
-        }
+  const handleOnChangeName = () => {
+    if (socket) {
+      socket.emit("set_name", {
+        userId: user.userId,
+        name: name,
+      });
+    }
+  };
+
+  const handleOnBell = () => {
+    if (socket) {
+      socket.emit("push_bell", { userId: user.userId });
+    }
+  };
+
+  useEffect(() => {
+    if (song.status === "stop") {
+      if (audioRef.current) {
+        setSongSrc("");
+        audioRef.current.pause();
+
+        setTimeout(() => {
+          audioRef.current.load();
+        }, 300);
+      }
+      return;
     }
 
-    const handleOnBell = () => {
-        if(socket) {
-            socket.emit("push_bell", {userId: user.userId})
+    if (song.position) {
+      setSongSrc("");
+      audioRef.current.load();
+
+      setTimeout(() => {
+        if (audioRef.current) {
+          setSongSrc(`./assets/songs/${song.position}.mp3`);
+          audioRef.current.load();
+          setTimeout(() => {
+            audioRef.current.play();
+            dispatch({
+              type: "push_bell",
+              payload: {
+                users: [],
+                userId: "",
+              },
+            });
+          }, 300);
         }
+      }, 300);
     }
+  }, [song]);
+  console.log(users);
 
-    useEffect(() => {
-        if(song.status === "stop") {
-            if(audioRef.current) {
-                setSongSrc("");
-                audioRef.current.pause();
-
-                setTimeout(() => {
-                    audioRef.current.load();
-                }, 300)
-            }
-            return;
-        }
-
-        if(song.position) {
-            setSongSrc("");
-            audioRef.current.load();
-
-            setTimeout(() => {
-                if(audioRef.current) {
-                    setSongSrc(`./assets/songs/${song.position}.mp3`);
-                    audioRef.current.load();
-                    setTimeout(() => {
-                        audioRef.current.play();
-                        dispatch({
-                            type: "push_bell",
-                            payload: {
-                                users: [],
-                                userId: "",
-                            }
-                        })
-                    }, 300)
-                }
-            }, 300)
-
-        }
-    }, [song])
-
-    return (
-        <div>
-            <h2>Socket IO Information:</h2>
-            <p>
-                Your user ID: <strong>{user.userId}</strong>
-                <br />
-                Your user name: <strong>{user.name}</strong>
-                <br />
-                Users online: <strong>{users.length}</strong>
-                <br />
-                Socket ID: <strong>{socket?.id}</strong>
-                <br />
-                Song Position: <strong>{song.position}</strong>
-                <br />
-                Song Status: <strong>{song.status}</strong>
-                <br />
-            </p>
-
-            <span>List User: </span>
-            {users.map((user) => (
-                <span key={user.userId}>{user.name}, </span>
-            ))}
-
-            <br/>
-            Masukan nama:
-            <input type="text" onChange={(e) => setName(e.target.value)} />
-            <button onClick={handleOnChangeName}>Ganti Nama</button>
-
-            <br />
-            < div >
-            <audio controls ref={audioRef}>
-                <source
-                    src={songSrc}
-                    type="audio/mp3"
-                />
-            </audio>
-            </div >
-
-            <br />
-            {pushedBell ? (
-                <p>Pemencet Bell: {pushedBell}</p>
-            ) : (
-                <button onClick={handleOnBell}>Pencet Bell</button>
-            )}
-        </div>
-    );
+  return (
+    <div>
+      <h2>Socket IO Information:</h2>
+      <p>
+        Your user ID: <strong>{user.userId}</strong>
+        <br />
+        Your user name: <strong>{user.name}</strong>
+        <br />
+        Users online: <strong>{users.length}</strong>
+        <br />
+        Socket ID: <strong>{socket?.id}</strong>
+        <br />
+        Song Position: <strong>{song.position}</strong>
+        <br />
+        Song Status: <strong>{song.status}</strong>
+        <br />
+      </p>
+      <span>List User: </span>
+      {users.map((user) => (
+        <span key={user.userId}>{user.name}, </span>
+      ))}
+      <br />
+      Masukan nama:
+      <input type="text" onChange={(e) => setName(e.target.value)} />
+      <button onClick={handleOnChangeName}>Ganti Nama</button>
+      <br />
+      <div>
+        <audio controls ref={audioRef}>
+          <source src={songSrc} type="audio/mp3" />
+        </audio>
+      </div>
+      <br />
+      {pushedBell ? (
+        <p>Pemencet Bell: {pushedBell}</p>
+      ) : (
+        <button onClick={handleOnBell}>Pencet Bell</button>
+      )}
+    </div>
+  );
 };
 
 export default Application;
--- src/context/SocketContext.ts
+++ src/context/SocketContext.ts
@@ -1,95 +1,130 @@
-import { createContext } from 'react';
-import { Socket } from 'socket.io-client';
+import { createContext } from "react";
+import { Socket } from "socket.io-client";
 
 export type UserType = {
-    socketId: string;
-    userId: string;
-    name: string;
-}
+  socketId: string;
+  userId: string;
+  name: string;
+  score: number;
+};
 
 export interface ISocketContextState {
-    socket: Socket | undefined;
-    users: UserType[];
-    user: UserType;
-    song: SongType;
-    pushedBell: string;
+  socket: Socket | undefined;
+  users: UserType[];
+  user: UserType;
+  song: SongType;
+  pushedBell: string;
 }
 
 export type SongType = {
-    position: string;
-    status: string;
-}
-
-export const defaultSocketContextState: ISocketContextState = {
-    socket: undefined,
-    users: [],
-    user: {
-        socketId: "",
-        userId: "",
-        name: "",
-    },
-    song: {
-        position: "",
-        status: "",
-    },
-    pushedBell: "",
+  position: string;
+  status: string;
 };
 
-export type TSocketContextActions = 'update_socket' | 'update_uid' | 'update_users' | 'remove_user' | 'set_song' | 'set_name' | 'push_bell';
+export const defaultSocketContextState: ISocketContextState = {
+  socket: undefined,
+  users: [],
+  user: {
+    socketId: "",
+    userId: "",
+    name: "",
+    score: 0,
+  },
+  song: {
+    position: "",
+    status: "",
+  },
+  pushedBell: "",
+};
+
+export type TSocketContextActions =
+  | "update_socket"
+  | "update_uid"
+  | "update_users"
+  | "remove_user"
+  | "set_song"
+  | "set_name"
+  | "push_bell"
+  | "set_score";
 export type TSocketContextPayload = any;
 
 export interface ISocketContextActions {
-    type: TSocketContextActions;
-    payload: TSocketContextPayload;
+  type: TSocketContextActions;
+  payload: TSocketContextPayload;
 }
 
-export const SocketReducer = (state: ISocketContextState, action: ISocketContextActions) => {
-    switch (action.type) {
-        case 'update_socket':
-            return { ...state, socket: action.payload as Socket };
-        case 'update_uid':
-            return { 
-                ...state,
-                user: {
-                    ...state.user,
-                    userId: action.payload as string 
-                }
-            };
-        case 'update_users':
-            return { ...state, users: action.payload as UserType[] };
-        case 'remove_user':
-            return { ...state, users: state.users.filter((user) => user.userId !== (action.payload as string)) };
-        case 'set_song':
-            return { ...state, song: JSON.parse(action.payload as string) as SongType };
-        case 'set_name':
-            return { 
-                ...state,
-                users: action.payload.users,
-                ...(state.user.userId === action.payload.userId && {
-                    user: {
-                        ...state.user,
-                        name: action.payload.name
-                    }
-                })
-            };
-        case 'push_bell':
-            return { 
-                ...state,
-                pushedBell: (action.payload.users as UserType[]).find((user) => user.userId === action.payload.userId)?.name || ""
-            };
-        default:
-            return state;
-    }
+export const SocketReducer = (
+  state: ISocketContextState,
+  action: ISocketContextActions
+) => {
+  switch (action.type) {
+    case "update_socket":
+      return { ...state, socket: action.payload as Socket };
+    case "update_uid":
+      return {
+        ...state,
+        user: {
+          ...state.user,
+          userId: action.payload as string,
+        },
+      };
+    case "update_users":
+      return { ...state, users: action.payload as UserType[] };
+    case "remove_user":
+      return {
+        ...state,
+        users: state.users.filter(
+          (user) => user.userId !== (action.payload as string)
+        ),
+      };
+    case "set_song":
+      return {
+        ...state,
+        song: JSON.parse(action.payload as string) as SongType,
+      };
+    case "set_name":
+      return {
+        ...state,
+        users: action.payload.users,
+        ...(state.user.userId === action.payload.userId && {
+          user: {
+            ...state.user,
+            name: action.payload.name,
+          },
+        }),
+      };
+    case "push_bell":
+      return {
+        ...state,
+        pushedBell:
+          (action.payload.users as UserType[]).find(
+            (user) => user.userId === action.payload.userId
+          )?.name || "",
+      };
+    case "set_score":
+      return {
+        ...state,
+        users: action.payload.users,
+        ...(state.user.userId === action.payload.userId && {
+          user: {
+            ...state.user,
+            score: action.payload.score,
+          },
+        }),
+      };
+    default:
+      return state;
+  }
 };
 
 export interface ISocketContextProps {
-    SocketState: ISocketContextState;
-    SocketDispatch: React.Dispatch<ISocketContextActions>;
+  SocketState: ISocketContextState;
+  SocketDispatch: React.Dispatch<ISocketContextActions>;
 }
 
 const SocketContext = createContext<ISocketContextProps>({
-    SocketState: defaultSocketContextState,
-    SocketDispatch: () => {}
+  SocketState: defaultSocketContextState,
+  SocketDispatch: () => {},
 });
 
 export const SocketContextConsumer = SocketContext.Consumer;
